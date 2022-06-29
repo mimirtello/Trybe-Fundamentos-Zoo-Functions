ee7bf004d0ca646d3ab7b596b6142920339b11b8
Yasmin Reis Tello <mimirt08@gmail.com>
2022-06-29 18:06:55 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui ...
+function getSpeciesByIds(...ids) {
+  let idAnimal = ids;
+  if (idAnimal === '') {
+    idAnimal = [];
+    return idAnimal;
+  }
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '0938aa23-f153-4937-9f88-4858b24d6bce')
 module.exports = getSpeciesByIds;
