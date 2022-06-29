66625361e2a91f6698de518f331042d87c982c48
Yasmin Reis Tello <mimirt08@gmail.com>
2022-06-29 18:08:13 -0300

    Corrigindo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -10,5 +10,4 @@ function getSpeciesByIds(...ids) {
   return species.filter((specie) => ids.includes(specie.id));
 }
 
-getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '0938aa23-f153-4937-9f88-4858b24d6bce')
 module.exports = getSpeciesByIds;
