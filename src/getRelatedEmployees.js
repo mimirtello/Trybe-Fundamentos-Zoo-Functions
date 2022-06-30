d5b4c06824432f07a89a7d4ed3b58342bc910453
Yasmin Reis Tello <mimirt08@gmail.com>
2022-06-30 11:59:29 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const array = [];
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  employees.forEach((item, index) => {
+    if (employees[index].managers.includes(managerId)) {
+      array.push(`${employees[index].firstName} ${employees[index].lastName}`);
+    }
+  });
+  return array;
 }
 
 module.exports = { isManager, getRelatedEmployees };
