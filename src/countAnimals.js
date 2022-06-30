4fbca138a60fe11b7b3dbdddfad1bbc0fac8ea7b
Yasmin Reis Tello <mimirt08@gmail.com>
2022-06-30 13:38:28 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  const zooAnimal = species.find((specie) => specie.name === animal.specie);
+  return zooAnimal.residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
