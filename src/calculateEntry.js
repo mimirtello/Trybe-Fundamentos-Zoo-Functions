3746fed3bbea9e2e995673587ccbfad075863c63
Yasmin Reis Tello <mimirt08@gmail.com>
2022-06-30 15:27:11 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,18 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((crianca) => crianca.age < 18).length;
+  const adult = entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50).length;
+  const senior = entrants.filter((idoso) => idoso.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  return Object.keys(prices).reduce((acc, curr) => acc + (prices[curr] * countEntrants(entrants)[curr]), 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
