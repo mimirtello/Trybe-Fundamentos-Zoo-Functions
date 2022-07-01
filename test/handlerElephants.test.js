21a5fc2db269d64297c449a0cc96a3818bd725bf
Yasmin Reis Tello <mimirt08@gmail.com>
2022-07-01 13:54:19 -0300

    Requisito 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -12,14 +12,20 @@ describe('Testes da função HandlerElephants', () => {
   it('Parâmetro inválido, é necessário uma string', () => {
     expect(handlerElephants(5)).toEqual('Parâmetro inválido, é necessário uma string');
   });
-  it('Compute date', () => {
+  it('Compute data1', () => {
     expect(computeData('count')).toEqual(elephants.residents.length);
   });
-  it('Compute date2', () => {
+  it('Compute data2', () => {
     expect(computeData('names')).toEqual(elefantsMap);
   });
-  it('Computed date3', () => {
+  it('Compute data3', () => {
     const averageAge = ({ residents }) => residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
     expect(computeData('averageAge')).toEqual(averageAge(elephants));
+  });
+  it('Compute data - default', () => {
+    expect(computeData('default')).toEqual(null);
+  });
+  it('Popularidade ser igual a 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
   });
 });
